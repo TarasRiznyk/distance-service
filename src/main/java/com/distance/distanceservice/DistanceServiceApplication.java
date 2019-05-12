f315f24495dc5f53e882851a6a47bcf181126619
wonderbird <riznyk.taras@gmail.com>
2019-05-12 18:11:37 +0300

    Initial commit

---
+++ src/main/java/com/distance/distanceservice/DistanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distance.distanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistanceServiceApplication.class, args);
+    }
+
+}
